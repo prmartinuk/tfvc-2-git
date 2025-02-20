5b3dcafcefaec62e5110c0fec6939eeed98b7927
Martin, Paul <paul.martin@cosworth.com>
2025-02-20 15:53:27 +0000

    Handle migrating moved TFVC code
    Change the method of finding the parent branch to cope with situation where the code has been deleted or moved in TFVC.
    In this case the DevOps APIs will return the changeset id of when the parent branch was deleted/moved rather than the changeset when it was first created.

--- src/Tfvc2Git.Core/ActionHandlers/BranchingHandler.cs
+++ src/Tfvc2Git.Core/ActionHandlers/BranchingHandler.cs
@@ -11,34 +11,39 @@ using Tfvc2Git.Core.Repositories;
 
 namespace Tfvc2Git.Core.ActionHandlers
 {
-    public sealed class BranchingHandler : Tfvc2GitActionBase
+	public sealed class BranchingHandler : Tfvc2GitActionBase
     {
         public void Handle(Tfvc2GitRepository repository, BranchMap branchMap, HistoryEntry historyEntry, bool filterByHistory = true)
         {
             var branchFilter = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
             branchFilter.AddRange(repository.Config.Branches.Select(x => x.TfvcServerPath));
-            var currentTfvcBranchIdentifier = new ItemIdentifier(branchMap.TfvcServerPath);
-            var parentTfvcBranches = repository.Tfvc.Vcs
-                .QueryBranchObjects(currentTfvcBranchIdentifier, RecursionType.OneLevel)
-                .Where(x => branchFilter.Contains(x.Properties.RootItem.Item))
-                .ToArray();
-            if (parentTfvcBranches.Length > 1)
+
+            var parentRootChangesetId = -1;
+            ItemIdentifier sourceItemIdentifier = null;
+
+            ItemSpec[] itemSpecs = new ItemSpec[] { new ItemSpec(branchMap.TfvcServerPath, RecursionType.OneLevel) };
+            BranchHistoryTreeItem[][] branchHistoryTrees = repository.Tfvc.Vcs.GetBranchHistory(itemSpecs, new ChangesetVersionSpec(historyEntry.ChangesetId));
+            foreach (var branchHistoryTree in branchHistoryTrees)
             {
-                Log.Error(" - Multiples parent branches found for {ServerPath}!", branchMap.TfvcServerPath);
-                throw new ApplicationException($"Multiples parent branches found for {branchMap.TfvcServerPath} : {parentTfvcBranches.Length}");
+                foreach (var item in branchHistoryTree)
+                {
+                    FindParentBranch(branchMap, historyEntry, branchFilter, ref parentRootChangesetId, ref sourceItemIdentifier, item);
+
+                    if (sourceItemIdentifier != null)
+                    {
+                        break;
+                    }
+                }
+
+                if (sourceItemIdentifier != null)
+                {
+                    break;
+                }
             }
 
-            var parentTfvcBranch = parentTfvcBranches.SingleOrDefault()?.Properties.ParentBranch;
-            if (!parentTfvcBranches.Any())
+            if (sourceItemIdentifier != null)
             {
-                Log.Error(" - No parent branch found for {ServerPath}!", branchMap.TfvcServerPath);
-            }
-            else if (null != parentTfvcBranch)
-            {
-                var parentRootChangesetId = ((ChangesetVersionSpec) parentTfvcBranch.Version).ChangesetId;
-
-                var sourceItemIdentifier = new ItemIdentifier(parentTfvcBranch.Item);
-                var targetItemIdentifier = new[] {new ItemIdentifier(branchMap.TfvcServerPath)};
+                var targetItemIdentifier = new[] { new ItemIdentifier(branchMap.TfvcServerPath) };
                 var allChangesetIds = repository.Config.History
                     .Where(x => x.ChangesetId < historyEntry.ChangesetId)
                     .Where(x => x.ChangesetId >= parentRootChangesetId)
@@ -73,13 +78,53 @@ namespace Tfvc2Git.Core.ActionHandlers
                 if (mergeChangesetIds.Any())
                 {
                     var rootChangeset = mergeChangesetIds.Max();
-                    Handle(repository, historyEntry, rootChangeset, parentTfvcBranch.Item);
-                    return;
+                    Handle(repository, historyEntry, rootChangeset, sourceItemIdentifier.Item);
+                }
+                else
+                {
+                    Log.Warning(" - Unable to find a changeset, fallback to Initial commit {GitSha} as source", repository.InitialCommitSha);
+                    Handle(repository, historyEntry, repository.InitialCommitSha);
+                }
+            }
+            else
+            {
+                if (parentRootChangesetId > 0)
+                {
+                    Log.Warning(" - Source branch is not one of the filtered branches, fallback to Initial commit {GitSha} as source", repository.InitialCommitSha);
+                }
+                else
+                {
+                    Log.Error(" - No parent branch found for {ServerPath}!", branchMap.TfvcServerPath);
+                }
+                Handle(repository, historyEntry, repository.InitialCommitSha);
+            }
+        }
+
+        private static void FindParentBranch(BranchMap branchMap, HistoryEntry historyEntry, HashSet<string> branchFilter, ref int parentRootChangesetId, ref ItemIdentifier sourceItemIdentifier, BranchHistoryTreeItem item)
+        {
+            if ((item.Relative.BranchToItem.ServerItem == branchMap.TfvcServerPath) &&
+                (item.Relative.BranchToItem.ChangesetId == historyEntry.ChangesetId) &&
+                (item.Relative.BranchFromItem != null))
+            {
+                parentRootChangesetId = item.Relative.BranchFromItem.ChangesetId;
+                if (branchFilter.Contains(item.Relative.BranchFromItem.ServerItem))
+                {
+                    sourceItemIdentifier = new ItemIdentifier(item.Relative.BranchFromItem.ServerItem, new ChangesetVersionSpec(parentRootChangesetId));
                 }
             }
 
-            Log.Debug(" - No source changesetId found in history, fallback to Initial commit {GitSha} as source", repository.InitialCommitSha);
-            Handle(repository, historyEntry, repository.InitialCommitSha);
+            if (sourceItemIdentifier == null)
+            {
+                foreach (var child in item.Children.OfType<BranchHistoryTreeItem>())
+                {
+                    FindParentBranch(branchMap, historyEntry, branchFilter, ref parentRootChangesetId, ref sourceItemIdentifier, child);
+
+                    if (sourceItemIdentifier != null)
+                    {
+                        break;
+                    }
+                }
+            }
         }
 
         private void Handle(Tfvc2GitRepository repository, HistoryEntry historyEntry, string sha)
